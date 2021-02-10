c48888b2a20be5c6d051351cf99232defb2f43e8
lcs2001 <lcs2001_@hotmail.com>
2021-02-10 12:30:31 -0300

    Removendo pacote e biblioteca

--- listaExercicio01/ex02.java
+++ listaExercicio01/ex02.java
@@ -1,5 +1,3 @@
-import java.io.InputStreamReader;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
--- listaExercicio01/ex03.java
+++ listaExercicio01/ex03.java
@@ -1,5 +1,3 @@
-package com.lucasangelo;
-
 public class Main {
 
     public static void main(String[] args) {

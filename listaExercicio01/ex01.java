95959610703c1abad6353cedd4da0bc639da37f2
lcs2001 <lcs2001_@hotmail.com>
2021-02-10 12:28:29 -0300

    Lista1 para enviar sem pacotes

---
+++ listaExercicio01/ex01.java
@@ -0,0 +1,98 @@
+public class Main {
+
+    // As explicações para cada mudança estão na linha anterior de forma comentada
+
+    public static void main(String[] args) {
+        double notas[] = new double[3];
+
+        for(int i=0; i<notas.length; i++)
+            notas[i] = 25.00;
+        Aluno alunoTesteAprovado = new Aluno(notas, 0.75);
+        System.out.println(
+                verificarAprovacao(
+                        alunoTesteAprovado.getNotas(),
+                        alunoTesteAprovado.getFrequencia()
+                )
+        );
+
+        for(int i=0; i<notas.length; i++)
+            notas[i] = 25.00;
+        Aluno alunoTesteReprovado = new Aluno(notas, 0.74);
+        System.out.println(
+                verificarAprovacao(
+                        alunoTesteReprovado.getNotas(),
+                        alunoTesteReprovado.getFrequencia()
+                )
+        );
+
+        for(int i=0; i<notas.length; i++)
+            notas[i] = 15.00;
+        Aluno alunoTesteEmReavaliacao = new Aluno(notas, 0.75);
+        System.out.println(
+                verificarAprovacao(
+                        alunoTesteEmReavaliacao.getNotas(),
+                        alunoTesteEmReavaliacao.getFrequencia()
+                )
+        );
+
+    }
+
+    // Função ser privada pelo Fator de Integridade
+    // Nome da funcao alterada para Fator de Legibilidade
+    /* Uso de parâmetros para Fator de Modularização (Por meio da classe Aluno)
+    Integridade (segurança dos dados não saber mais que o necessário). Com isso é possível fazer testes
+    para o Fator de Verificabilidade */
+    private static String verificarAprovacao(double nota[], double frequencia) {
+
+        // Zerar variável para garantir que não exista lixo da memória e adequar ao Fato de Correção
+        // Somatório da notaFinal ser double para o Fator de Robustez caso a nota não seja exata
+        double notaFinal=0;
+
+        // Com finalidade de aumentar a Legibilidade, retornar o que ocorreu diretamente com o aluno
+        // Talvez isso impactaria minimamente no fator de Eficiência, pois uma String gasta mais que int
+        String status = new String();
+
+        // Somar dinamicamente para garantir o Fator de Reusabilidade dessa função caso aja necessidade
+        for(int i=0; i<nota.length; i++)
+            notaFinal+=nota[i];
+
+        /* Com objetivo de diminuir a quantiade de comparacões, utilizei o frequência como condição principal,
+        já que se a frequência for menor que 0.75 não precisa fazer mais comparações, está reprovado. */
+        // Mudança feito para agregar um mínimo Fator de Eficiência
+        if(frequencia<0.75)
+            status="Reprovado";
+        else
+            if(notaFinal>=60)
+                status="Aprovado";
+            else
+                status="Reavalição";
+
+        return status;
+    }
+}
+
+class Aluno {
+    private double notas[];
+    private double frequencia;
+
+    public Aluno(double[] notas, double frequencia) {
+        this.notas = notas;
+        this.frequencia = frequencia;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double getFrequencia() {
+        return frequencia;
+    }
+
+    public void setFrequencia(double frequencia) {
+        this.frequencia = frequencia;
+    }
+}
---
+++ listaExercicio01/ex03.java
@@ -0,0 +1,60 @@
+package com.lucasangelo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double notas[] = new double[4];
+
+        for(int i=0; i<notas.length; i++)
+            notas[i] = 10;
+	    Turma turma1 = new Turma(notas);
+        System.out.println("Média turma 1: " + calcularMedia(turma1.notas));
+
+        for(int i=0; i<notas.length; i++)
+            notas[i] = 7;
+        Turma turma2 = new Turma(notas);
+        System.out.println("Média turma 2: " + calcularMedia(turma2.notas));
+
+        for(int i=0; i<notas.length; i++)
+            notas[i] = 5;
+        Turma turma3 = new Turma(notas);
+        System.out.println("Média turma 3: " + calcularMedia(turma3.notas));
+
+        double semNotas[] = new double[0];
+        for(int i=0; i<0; i++)
+            semNotas[i] = 5;
+        Turma turma4 = new Turma(semNotas);
+        System.out.println("Média turma 4: " + calcularMedia(turma4.notas));
+    }
+
+    // Função privada por fator de Integridade
+    // Função recebe por parâmetro os dados que podem ser enviados de forma modularizada
+    // Função retornando a média em double, com isso aumentando a Modularidade podendo tratar o retorno
+    private static double calcularMedia(double notas[]){
+        double mediaTurma;
+        // Para tratar a exessão de divisão por 0, aumentando Fator de Robustez
+        if(notas.length!=0) {
+            // Por Fator de Robustez, notas e somas são em double para caso exista uma nota não exata
+            double somatorio = 0;
+            // Apenas um for pois agora é Modularizado
+            for (int i = 0; i < notas.length; i++) {
+                somatorio += notas[i];
+            }
+            // Casting double para aumentar o Fator de Robustez
+            mediaTurma = somatorio / ((double) notas.length);
+        } else {
+            mediaTurma=0;
+            System.out.print("Não é possível efetuar a média sem notas! - ");
+        }
+
+        return mediaTurma;
+    }
+}
+
+class Turma {
+    double notas[];
+
+    public Turma(double notas[]){
+        this.notas = notas;
+    }
+}
